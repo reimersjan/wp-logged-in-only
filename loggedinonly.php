eeb9c56dcf613d6616e8aa53072232a7eadca375
Jan Reimers <me@janreimers.net>
2014-02-15 13:42:04 +0100

    updated version

--- loggedinonly.php
+++ loggedinonly.php
@@ -4,7 +4,7 @@ Plugin Name: Logged-in-only
 Plugin URI: https://github.com/reimersjan/wp-logged-in-only
 Description: Lock down the whole site to prevent public access. Only logged-in users can view the site if this plugin is activated.
 Author: Jan Reimers
-Version: 1.0
+Version: 1.0.1
 Author URI: http://www.jan.rs
 License: GPLv2
 */

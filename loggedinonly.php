12bbc2e83d07873058a681cf74bf1966835adf6a
Ralf Wiechers <drivingralle@me.com>
2014-07-11 00:18:45 +0200

    Some codestyle

--- loggedinonly.php
+++ loggedinonly.php
@@ -10,9 +10,8 @@ License: GPLv2
 */
 
 function logged_in_only() {
-    if ( !is_user_logged_in() ) {
-        auth_redirect();
-    }
+	if ( ! is_user_logged_in() ) {
+		auth_redirect();
+	}
 }
-add_action ('template_redirect', 'logged_in_only');
-?>
+add_action( 'template_redirect', 'logged_in_only' );
